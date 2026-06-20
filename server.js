33e47b0fa175710c33ac173230c55c7c106fe670
“Nilde <nilde.benedetti@gmail.com>
2026-06-20 10:18:17 +0200

    fix ordine logico dei middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import categoriesRouter from "./routers/categories";
 import powersRouter from "./routers/powers";
+import notFound from "./middlewares/notFound";
 
 const port = process.env.SERVER_PORT || 3000;
 
@@ -12,14 +13,15 @@ app.use(express.json());
 app.use("/categories", categoriesRouter);
 app.use("/powers", powersRouter);
 
-app.use(notFound);
-app.use(errorHandler);
-
 app.get("/", (request, response) => {
     response.json({
         message: 'il server funziona correttamente'
     })
 });
+
+app.use(notFound);
+app.use(errorHandler);
+
 
 app.listen(port, (error) => {
     if (error) {
