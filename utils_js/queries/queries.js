666f26c4cfaf538a9a95a9846d75e2054adf4726
“Nilde <nilde.benedetti@gmail.com>
2026-06-21 11:22:28 +0200

    aggiunto file per variabili queries

---
+++ utils_js/queries/queries.js
@@ -0,0 +1,11 @@
+/* inserire tutte le query per avere più ordine nel codice! 
+usiamo la sintassi */
+
+/*======== PRODUCTS ========*/
+
+querySelectAllProducts = `
+inserisci qui la query`;
+
+export {
+    querySelectAllProducts
+};
