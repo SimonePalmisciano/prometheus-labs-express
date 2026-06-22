9dadf71de4f27cfa22d269431e235b261b36a6aa
SimonePalmisciano <simonepalmisciano11@gmail.com>
2026-06-22 17:48:20 +0200

    inizializzata validationOrder

---
+++ middlewares/validationOrder.js
@@ -0,0 +1,13 @@
+function validationOrder(request, response, next) {
+    const {
+        guest_email,
+        guest_name,
+        guest_surname,
+        city,
+        country,
+        total_amount,
+        items
+    } = req.body;
+
+
+}
